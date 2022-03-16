586c942055f1b007f8ee35c686c38a7e2e3bc02b
davylander <101679754+davylander@users.noreply.github.com>
2022-03-16 20:23:12 -0300

    iniciando etapa 2 do curso java brocode, começando methods

---
+++ src/brocodemetodosprafrente/Methods.java
@@ -0,0 +1,10 @@
+package brocodemetodosprafrente;
+
+public class Methods {
+
+	public static void main(String[] args) {
+		// method = a block of code that is executed whenever it is called upon
+
+	}
+
+}
