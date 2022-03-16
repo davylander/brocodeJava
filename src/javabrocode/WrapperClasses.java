5a0cb918c77614faa7f683871437a115ced6bed7
davylander <101679754+davylander@users.noreply.github.com>
2022-03-15 21:38:27 -0300

    Adicionando inicio da aula de Wrapper Classes

---
+++ src/javabrocode/WrapperClasses.java
@@ -0,0 +1,12 @@
+package javabrocode;
+
+public class WrapperClasses {
+
+	public static void main(String[] args) {
+		// wrapper class = provides a way to use primitive data types as reference data types
+		//					reference data types contain useful methods
+		//					can be used with collections (ex.ArrayList)
+
+	}
+
+}
