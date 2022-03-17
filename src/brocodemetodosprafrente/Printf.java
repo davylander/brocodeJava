c64dc91b53db5920fbd9d0223f9b22dba2567aed
davylander <101679754+davylander@users.noreply.github.com>
2022-03-16 21:02:00 -0300

    BroCode Java E2: Overloaded Methods finalizado. Iniciando Printf

---
+++ src/brocodemetodosprafrente/Printf.java
@@ -0,0 +1,12 @@
+package brocodemetodosprafrente;
+
+public class Printf {
+
+	public static void main(String[] args) {
+		// printf() = an optional method to control, format, and display text to the console window
+		// 			  two arguments = format string + (object/variable/ value)
+		//			  % [flags] [precision] [width] [conversion-character]
+
+	}
+
+}
