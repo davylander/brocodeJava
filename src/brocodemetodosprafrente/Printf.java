626a3a030a2ac8b1b82f2486e7c765e6cbfa6292
davylander <101679754+davylander@users.noreply.github.com>
2022-03-16 21:11:53 -0300

    BroCode Java E2: Printf 50%. Amanhã continuo.

--- src/brocodemetodosprafrente/Printf.java
+++ src/brocodemetodosprafrente/Printf.java
@@ -7,6 +7,24 @@ public class Printf {
 		// 			  two arguments = format string + (object/variable/ value)
 		//			  % [flags] [precision] [width] [conversion-character]
 
+		System.out.printf("This is a format string %d", 123);
+		System.out.println();
+		System.out.printf("%d This is a format string \n", 123);
+		
+		System.out.println("__________________________________________ \n");
+		
+		boolean myBoolean = true;
+		char myChar = '@';
+		String myString = "Davi";
+		int myInt = 23;
+		double myDouble = 1.83;
+		
+		System.out.printf("%b \n", myBoolean);
+		System.out.printf("%c \n", myChar);
+		System.out.printf("%s \n", myString);
+		System.out.printf("%d \n", myInt);
+		System.out.printf("%f \n", myDouble);
+		
 	}
 
 }
