e1fa7504d9865509dd202e5419c3f35d74bad324
davylander <101679754+davylander@users.noreply.github.com>
2022-03-17 15:19:21 -0300

    BroCode Java E2: Final Keyword concluido. | Iniciando E3: Objetos

---
+++ src/brocodemetodosprafrente/FinalKeyword.java
@@ -0,0 +1,15 @@
+package brocodemetodosprafrente;
+
+public class FinalKeyword {
+
+	public static void main(String[] args) {
+		// final = cannot be changed or update later in the program.
+		
+		final double PI = 3.14159;
+		
+		
+		System.out.println(PI);
+
+	}
+
+}
