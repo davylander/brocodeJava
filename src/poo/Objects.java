80ada195a87fca3513954b2ad8b0fd510af43e7a
davylander <101679754+davylander@users.noreply.github.com>
2022-03-18 19:54:32 -0300

    BroCode Java E3: 26. Objects (POO) [concĺuído]

--- src/poo/Objects.java
+++ src/poo/Objects.java
@@ -3,8 +3,25 @@ package poo;
 public class Objects {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		// object = an instance of a class that may contain attributes and methods
+		// example: (phone, desk, computer, coffee cup)
+		
+		Car myCar1 = new Car();
+		Car myCar2 = new Car();
+		
+		System.out.println(myCar1.make);
+		System.out.println(myCar1.model);
+		
+		System.out.println();
+		
+		System.out.println(myCar2.make);
+		System.out.println(myCar2.model);
+		
+		//myCar1.drive();
+		//myCar1.brake();
+		
+		
+		
 	}
 
 }
