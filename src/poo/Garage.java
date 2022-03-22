736af5067aba4af7effcc38bed3ef2827e391482
davylander <101679754+davylander@users.noreply.github.com>
2022-03-22 18:24:55 -0300

    BroCode Java: 32. Object passing [concluído]

--- src/poo/Garage.java
+++ src/poo/Garage.java
@@ -2,4 +2,8 @@ package poo;
 
 public class Garage {
 
+	void park(Car car) {
+		System.out.println("The " + car.model + " is parked in the garage!");
+	}
+	
 }
--- src/poo/ObjectPassing.java
+++ src/poo/ObjectPassing.java
@@ -6,8 +6,11 @@ public class ObjectPassing {
 		
 		Garage garage = new Garage();
 		
-		Car car = new Car("BMW");
+		Car car1 = new Car("BMW");
+		Car car2 = new Car("Tesla");
 
+		garage.park(car1);
+		garage.park(car2);
 	}
 
 }
