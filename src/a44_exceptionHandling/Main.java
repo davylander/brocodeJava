e38cf49f0a451a2921735b66427f371ced4e4060
davylander <101679754+davylander@users.noreply.github.com>
2022-03-26 20:53:46 -0300

    BroCode Java: 44. Exception Handling (Iniciando)

---
+++ src/a44_exceptionHandling/Main.java
@@ -0,0 +1,22 @@
+/*
+ * Classe para aula 44 . Exception Handlig - Main
+ * 26/03/2022
+ * Direitos reservados a Davi Souto & BroCode
+ */
+
+package a44_exceptionHandling;
+
+/*
+ * @version 1.0
+ * @author Davi Souto Neri - BroCode Bootcamp Java
+ */
+
+public class Main {
+	
+	public static void main(String[] args) {
+	
+		// exception = an event that occurs during the execution of a program that,
+		//			   disrupts the normal flow of intructions 
+		
+	}
+}
