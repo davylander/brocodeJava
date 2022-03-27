65075456c7332debae811c372f66402f82d8dd0c
davylander <101679754+davylander@users.noreply.github.com>
2022-03-27 17:06:06 -0300

    BroCode Java: 44. Exception Handling (Concluído)

--- src/a44_exceptionHandling/Main.java
+++ src/a44_exceptionHandling/Main.java
@@ -6,6 +6,9 @@
 
 package a44_exceptionHandling;
 
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
 /*
  * @version 1.0
  * @author Davi Souto Neri - BroCode Bootcamp Java
@@ -17,6 +20,33 @@ public class Main {
 	
 		// exception = an event that occurs during the execution of a program that,
 		//			   disrupts the normal flow of intructions 
+		
+		Scanner scanner = new Scanner(System.in);
+		
+		try {
+			System.out.println("Enter a whole number to divide: ");
+			int x = scanner.nextInt();
+			
+			System.out.println("Enter a whole number to divide by: ");
+			int y = scanner.nextInt();
+			
+			int z = x/y;
+			
+			System.out.println("Result: " + z);
+		}
+		catch(ArithmeticException e) {
+			System.out.println("You can't divide by zero! IDIOT!");
+		}
+		catch(InputMismatchException e) {
+			System.out.println("Enter a number!! DUMB");
+		}
+		catch(Exception e) {
+			System.out.println("Something went wrong!");
+		}
+		finally {
+			//System.out.println("This will always print");
+			scanner.close();
+		}
 		
 	}
 }
