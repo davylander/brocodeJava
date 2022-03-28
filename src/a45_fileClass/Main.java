25e7beda5dc6342b1dca245037e28732f634b992
davylander <101679754+davylander@users.noreply.github.com>
2022-03-28 14:56:56 -0300

    BroCode Java: 45. File Class (Concluído)

---
+++ src/a45_fileClass/Main.java
@@ -0,0 +1,36 @@
+/*
+ * Classe para aula 45. File class - Main
+ * 28/03/2022
+ * Todos os direitos reservados a Davi Souto e BroCode
+ */
+
+package a45_fileClass;
+
+import java.io.File;
+
+/*
+ * @version 1.0
+ * @author Davi Souto Neri - BroCode Bootcamp Java
+ */
+
+public class Main {
+
+	public static void main(String[] args) {
+		// file = An abstract representation of file and directory pathnames
+		
+		File file = new File("secret_message.txt");
+		
+		if(file.exists()) {
+			System.out.println("That file exists! :O");
+			System.out.println(file.getPath());
+			System.out.println(file.getAbsolutePath());
+			System.out.println(file.isFile());
+			file.delete();
+		}
+		else {
+			System.out.println("That fle doesn't exist! :(");
+		}
+
+	}
+
+}
