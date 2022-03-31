154ff6b927c402eefd014347102ae16f3bd4bd4a
davylander <101679754+davylander@users.noreply.github.com>
2022-03-31 20:37:54 -0300

    BroCode Java: 53. BorderLayout (Concluído)

---
+++ src/a53_border_layout/Main.java
@@ -0,0 +1,96 @@
+/*
+ * Classe para aula 53. BorderLayout - Main
+ * 31/03/2022
+ * Direitos reservados para Davi Souto & BroCode
+ */
+
+package a53_border_layout;
+
+import java.awt.BorderLayout;
+import java.awt.Color;
+import java.awt.Dimension;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+
+/*
+ * @version 1.0
+ * @author Davi Souto Neri - BroCode Bootcamp Java
+ */
+
+public class Main {
+	
+	public static void main(String[] args) {
+		/* Layout Manager = Defines the natural layout for components within a container 
+		 * 
+		 *  3 common managers
+		 *  
+		 *  BorderLayout = A BorderLayout places components in five areas: NOTH, SOUTH, WEST, EAST, CENTER.
+		 *  			   All extra space is placed in the center area.
+		 */
+		//____________________________________________________FRAME____________________________________________________
+		
+		JFrame frame = new JFrame();
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.setSize(500,500);
+		frame.setLayout(new BorderLayout(3,3)); // margem dos componentes  
+		frame.setVisible(true);
+		
+		//____________________________________________________PANEL____________________________________________________
+		
+		JPanel panel1 = new JPanel();
+		JPanel panel2 = new JPanel();
+		JPanel panel3 = new JPanel();
+		JPanel panel4 = new JPanel();
+		JPanel panel5 = new JPanel();
+		
+		panel1.setBackground(Color.red);
+		panel2.setBackground(Color.green);
+		panel3.setBackground(Color.yellow);
+		panel4.setBackground(Color.magenta);
+		panel5.setBackground(Color.blue);
+		
+		panel1.setPreferredSize(new Dimension(100,50));
+		panel2.setPreferredSize(new Dimension(50,100));
+		panel3.setPreferredSize(new Dimension(50,100));
+		panel4.setPreferredSize(new Dimension(100,50));
+		panel5.setPreferredSize(new Dimension(100,100));
+		
+		//________________________________________________SUB-PANEL____________________________________________________
+		
+		JPanel panel6 = new JPanel();
+		JPanel panel7 = new JPanel();
+		JPanel panel8 = new JPanel();
+		JPanel panel9 = new JPanel();
+		JPanel panel10 = new JPanel();
+		
+		panel6.setBackground(Color.black);
+		panel7.setBackground(Color.darkGray);
+		panel8.setBackground(Color.gray);
+		panel9.setBackground(Color.lightGray);
+		panel10.setBackground(Color.white);
+		
+		panel5.setLayout(new BorderLayout());
+		
+		panel6.setPreferredSize(new Dimension(50,50));
+		panel7.setPreferredSize(new Dimension(50,50));
+		panel8.setPreferredSize(new Dimension(50,50));
+		panel9.setPreferredSize(new Dimension(50,50));
+		panel10.setPreferredSize(new Dimension(50,50));
+		
+		panel5.add(panel6,BorderLayout.NORTH);
+		panel5.add(panel7,BorderLayout.WEST);
+		panel5.add(panel8,BorderLayout.EAST);
+		panel5.add(panel9,BorderLayout.SOUTH);
+		panel5.add(panel10,BorderLayout.CENTER);
+		
+		//_____________________________________________________________________________________________________________
+		
+		frame.add(panel1,BorderLayout.NORTH);
+		frame.add(panel2,BorderLayout.WEST);
+		frame.add(panel3,BorderLayout.EAST);
+		frame.add(panel4,BorderLayout.SOUTH);
+		frame.add(panel5,BorderLayout.CENTER);
+		
+	}
+}
