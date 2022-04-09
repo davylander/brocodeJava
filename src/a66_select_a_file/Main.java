24ff7a2f5843ac52ecac4e85673febe2dd044433
davylander <101679754+davylander@users.noreply.github.com>
2022-04-09 18:16:48 -0300

    BroCode Java: 66. Select a File (Concluído)

---
+++ src/a66_select_a_file/Main.java
@@ -0,0 +1,23 @@
+/*
+ * Classe da aula 66. Select a file - Main
+ * 09/04/2022
+ * Direitos reservados a Davi Souto & BroCode
+ */
+
+package a66_select_a_file;
+
+/*
+ * @version 1.0
+ * @author Davi Sout Neri - BroCode Bootcamp Java
+ */
+
+public class Main {
+
+	public static void main(String[] args) {
+		// JFileChooser = A GUI mechanism that let's a user choose a file (helpful for opening or saving files)
+
+		new MyFrame();
+		
+	}
+
+}
