8f56d804695c2e3dc70676defdcb8455f9e468ac
davylander <101679754+davylander@users.noreply.github.com>
2022-04-12 20:57:29 -0300

    BroCode Java: 71. Key Bindings (Concluído)

---
+++ src/a71_key_bindings/Game.java
@@ -0,0 +1,86 @@
+/*
+ * Classe para aula 71. Key Bindings - Game
+ * 12/04/2022
+ * Direitos reservados a Davi Souto & BroCode
+ */
+
+package a71_key_bindings;
+
+import java.awt.Color;
+import java.awt.event.*;
+
+import javax.swing.*;
+
+/*
+ * @version 1.0
+ * @author Davi Souto Neri - BroCode Bootcamp Java
+ */
+
+public class Game {
+
+	JFrame frame;
+	JLabel label;
+	Action upAction;
+	Action downAction;
+	Action leftAction;
+	Action rightAction;
+	
+	Game(){
+		
+		frame = new JFrame("KeyBinding Demo");
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.setSize(420,420);
+		frame.setLayout(null);
+		
+		label = new JLabel();
+		label.setBackground(Color.red);
+		label.setBounds(100,100,100,100);
+		label.setOpaque(true);
+		
+		upAction = new UpAction();
+		downAction = new DownAction();
+		leftAction = new LeftAction();
+		rightAction = new RightAction();
+		
+		label.getInputMap().put(KeyStroke.getKeyStroke("UP"), "upAction");
+		label.getActionMap().put("upAction", upAction);
+		label.getInputMap().put(KeyStroke.getKeyStroke("DOWN"), "downAction");
+		label.getActionMap().put("downAction", downAction);
+		label.getInputMap().put(KeyStroke.getKeyStroke("LEFT"), "leftAction");
+		label.getActionMap().put("leftAction", leftAction);
+		label.getInputMap().put(KeyStroke.getKeyStroke("RIGHT"), "rightAction");
+		label.getActionMap().put("rightAction", rightAction);
+		
+		frame.add(label);
+		frame.setVisible(true);
+	}
+	
+	public class UpAction extends AbstractAction{
+		@Override
+		public void actionPerformed(ActionEvent e) {
+			label.setLocation(label.getX(),label.getY()-10);
+		}
+	}
+	
+	public class DownAction extends AbstractAction{
+		@Override
+		public void actionPerformed(ActionEvent e) {
+			label.setLocation(label.getX(),label.getY()+10);
+		}
+	}
+	
+	public class LeftAction extends AbstractAction{
+		@Override
+		public void actionPerformed(ActionEvent e) {
+			label.setLocation(label.getX()-10,label.getY());
+		}
+	}
+	
+	public class RightAction extends AbstractAction{
+		@Override
+		public void actionPerformed(ActionEvent e) {
+			label.setLocation(label.getX()+10,label.getY());
+		}
+	}
+	
+}
---
+++ src/a71_key_bindings/Main.java
@@ -0,0 +1,30 @@
+/*
+ * Classe para aula 71. Key Bindings - Main
+ * 12/04/2022
+ * Direitos reservados a Davi Souto & BroCode
+ */
+
+package a71_key_bindings;
+
+/*
+ * @version 1.0
+ * @author Davi Souto Neri - BroCode Bootcamp Java
+ */
+
+public class Main {
+
+	public static void main(String[] args) {
+		/* 
+		 * Key Bindings = bind an Action to a KeyStroke 
+		 * 				  dont't require you to click a component to give it focus
+		 * 				  all Swing components use Key Bindings
+		 * 				  increased flexibility compared to KeyListeners
+		 * 				  can assign key strokes to individual Swing components
+		 * 				  more difficult to utilize and set up :(
+		 */
+
+		Game game = new Game();
+		
+	}
+
+}
