e46b7f0dc2362bb8b9ab6cb548b95655d3183ed8
davylander <101679754+davylander@users.noreply.github.com>
2022-04-13 18:10:59 -0300

    BroCode Java: 74. Generics (Concluido), 74. Generics Classes (10:23:34)

---
+++ src/a74_generic_classes/Main.java
@@ -0,0 +1,25 @@
+/*
+ * Classe para aula 74. Generics Classes - Main
+ * 13/04/2022
+ * Direitos reservados a Davi Souto & BroCode
+ */
+
+package a74_generic_classes;
+
+/*
+ * @version 1.0
+ * @author Davi Souto Neri - BroCode Bootcamp Java
+ */
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		MyIntergerClass myInt = new MyIntegerClass(1);
+		MyDoubleClass myDouble = new MyDoubleClass(3.14);
+		MyCharacterClass myChar = new MyCharacterClass'@');
+		MyStringClass myString = new MyStringClass("Hello");
+		
+	}
+
+}
---
+++ src/a74_generic_classes/MyCharacterClass.java
@@ -0,0 +1,5 @@
+package a74_generic_classes;
+
+public class MyCharacterClass {
+
+}
