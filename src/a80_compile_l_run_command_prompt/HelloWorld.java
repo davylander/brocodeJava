a9d3fee422d5fe7e30446bbd325e89b214736662
davylander <101679754+davylander@users.noreply.github.com>
2022-04-20 18:43:43 -0300

    BroCode Java: 80. Compile/Run Command prompot (Concluído)

---
+++ src/a80_compile_l_run_command_prompt/HelloWorld.java
@@ -0,0 +1,7 @@
+package a80_compile_l_run_command_prompt;
+
+public class HelloWorld {
+	public static void main(String[] args) {
+		System.out.println("Hello World!!");
+	}
+}
---
+++ src/a80_compile_l_run_command_prompt/Main.java
@@ -0,0 +1,32 @@
+/*
+ * Classe para aula 80. Compile/Run command prompt - Main
+ * 20/04/2022
+ * Direitos reservados a Davi Souto & BroCode
+ */
+
+package a80_compile_l_run_command_prompt;
+
+/*
+ * @version 1.0
+ * @author Davi Souto Neri - BroCode Bootcamp Java
+ */
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		/*
+		 * Compile and run Java Command Prompt
+		 * _____________________________________
+		 * 1. Make sure you have a Java JDK installed (we did this lesson #1)
+		 * 2. (optional) use a text editor and save a file as .java
+		 * 3. Open COmmand Prompt (Windows) or Terminal (Mac) on your computer
+		 * 4. set path=C:\Program Files\Java\jdk-13.0.1\bin (where JDK is located)
+		 * 5. cd C:\Users\Cakow\Desktop (or wherever your java file is)
+		 * 6. javac HelloWorld.java (to compile)
+		 * 7. java HelloWorld (to run a .class filem it's potable)
+		 */
+		
+	}
+
+}
