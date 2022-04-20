71c7ea48fafd033bf9a17cecb2c6b92ccd6e10cf
davylander <101679754+davylander@users.noreply.github.com>
2022-04-20 14:02:47 -0300

    BroCode Java: 79. Packages (Concluído)

---
+++ src/a79_packages/Toolbox.java
@@ -0,0 +1,16 @@
+/*
+ * Classe para aula 78. Multithreading - Toolbox
+ * 20/04/2022
+ * Direitos reservados a Davi Souto & BroCode
+ */
+
+package a79_packages;
+
+/*
+ * @version 1.0
+ * @author Davi Souto Neri - BroCode Bootcamp Java
+ */
+
+public class Toolbox {
+
+}
---
+++ src/a79_packages_main/Main.java
@@ -0,0 +1,26 @@
+/*
+ * Classe para aula 78. Multithreading - Main
+ * 20/04/2022
+ * Direitos reservados a Davi Souto & BroCode
+ */
+
+package a79_packages_main;
+
+import a79_packages.Toolbox;
+
+import javax.swing.Icon;
+
+/*
+ * @version 1.0
+ * @author Davi Souto Neri - BroCode Bootcamp Java
+ */
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		Toolbox toolbox = new Toolbox();
+
+	}
+
+}
