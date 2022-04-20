36df18d0170a905d904117077ded156d320fdc22
davylander <101679754+davylander@users.noreply.github.com>
2022-04-20 19:55:35 -0300

    BroCode Java: 81. Executable (.jar) (Concluído). BroCode Java (Concluído :D)

---
+++ src/a81_executable_ponto_jar/Executable.java
@@ -0,0 +1,36 @@
+package a81_executable_ponto_jar;
+
+public class Executable {
+
+	public static void main(String[] args) {
+		/*
+		 * Create an executable jar with Eclipse IDE
+		 * --------------------------------------------------
+		 * 1. Right click on Java project (pick a project with a GUI)
+		 * 2. Export 
+		 * 3. Java > Runnable Jar file
+		 * 4. At Launch configuration  select your project
+		 * 5. At Export destination, select where you want this jar file exported to 
+		 * 6. Finish
+		 */
+		
+		/*
+		 *  If you want an icon, convert an image to a .ico file
+		 *  ---------------------------------------------------------
+			1. https://icoconvert.com/
+			(this is a decent site)
+			2. Choose your image (file type my vary)
+			3. Upload
+			4. Convert to ICO
+			5. Download your icon
+			
+			Use Launch4 to create an .exe with custom icon
+			--------------------------------------------------------
+			1. Install Launch4j http://launch4j.sourceforge.net/
+			2. Output file: browse for the output destination and name your file ending with .exe
+			3. Jar: browse for where you saved your jar or executable jar file
+		 */
+
+	}
+
+}
